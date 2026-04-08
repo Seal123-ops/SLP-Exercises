5b434bcd8c77eec3a75f5538eee7a4ccd8fcedde
Seal123-ops <naqia4@educbe.ca>
2026-04-08 14:00:11 -0600

    lo

---
+++ ex018/ConditionalStatements1.java
@@ -0,0 +1,16 @@
+public class ConditionalStatements1 {
+
+    public static String isNumberEven(int number) {
+    if (number % 2 == 0) {
+    return "even";
+    } 
+    else {
+    return "odd";
+
+    }
+            
+    }
+
+    
+    
+}
