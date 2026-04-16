0083a2db5f87c992a8d08d107f08d4de644d66f2
Seal123-ops <naqia4@educbe.ca>
2026-04-16 15:30:02 -0600

    l

--- ex019/ConditionalStatements2.java
+++ ex019/ConditionalStatements2.java
@@ -1,3 +1,15 @@
 public class ConditionalStatements2 {
    
+    public static String getSignum(int number) {
+        if(number < 0) {
+            return "negative";
+
+        } else if (number > 0) {
+            return "positive";
+        } else {
+            return "zero";
+        }
+    
+        
+    }
 }
--- ex020/ConditionalStatements3.java
+++ ex020/ConditionalStatements3.java
@@ -1,3 +1,9 @@
 public class ConditionalStatements3 {
+    public static String getAnimal(String animal) {
+        if (animal.equal("husky") || animal.equals("sheperd") || animal.equals("beagle")) {
+            return "dog";
+        } else if (animal.equals("siamese"))
+            
+    }
     
 }
